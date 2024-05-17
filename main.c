e387a2b3b4c25676ff331ca945afe1e90892703c
KisheDesiree <keann_lusong@dlsu.edu.ph>
2024-05-17 19:27:40 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
-int main() {
-  
+int main()
+
+{
+   char cName, cProduct;
+  float fPrice, fTotal;
+  int iQuantity;
+
+  printf("First letter of your name:");
+  scanf("%c", &cName);
+  printf("Welcome %c!\n", cName);
+
+  printf("What would you like to buy?\n");
+  scanf("%c", &cProduct);
+
+  printf("Price:\n");
+  scanf("%f", &fPrice);
+
+  printf("Quantity of products:");
+  scanf("%d", &iQuantity);
+
 return 0;
 }
+
 
